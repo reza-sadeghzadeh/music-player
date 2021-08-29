91a0d8d12c6f9e07c28f54e2659103d1e5117779
Reza <reza_sadeghzadeh01@yahoo.com>
2021-08-29 07:50:24 +0330

    Project cleaned up and some dependencies were installed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FaSmileWink } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FaSmileWink />
+      <h1>hello world</h1>
+    </>
   );
 }
 
